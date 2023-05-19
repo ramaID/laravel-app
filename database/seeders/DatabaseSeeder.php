b9e39acd11e449c2f2c1147edfdcb127a3e07647
rama <rama@javan.co.id>
2023-05-19 21:39:49 +0700

    Add database migration and seeding commands

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(500)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
