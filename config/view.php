bb162553507088b660eaa2630586a570c84d34c3
rama <rama@javan.co.id>
2023-05-20 19:24:40 +0700

    punchcard config

--- config/view.php
+++ config/view.php
@@ -1,36 +1,7 @@
 <?php
 
-return [
+declare(strict_types=1);
 
-    /*
-    |--------------------------------------------------------------------------
-    | View Storage Paths
-    |--------------------------------------------------------------------------
-    |
-    | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views. Of course
-    | the usual Laravel view path has already been registered for you.
-    |
-    */
+use TomasVotruba\PunchCard\ViewConfig;
 
-    'paths' => [
-        resource_path('views'),
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Compiled View Path
-    |--------------------------------------------------------------------------
-    |
-    | This option determines where all the compiled Blade templates will be
-    | stored for your application. Typically, this is within the storage
-    | directory. However, as usual, you are free to change this value.
-    |
-    */
-
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
-    ),
-
-];
+return ViewConfig::make()->toArray();
