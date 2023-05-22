99463553e18564cb8ea115cf3abcc88ae88fbeca
rama <rama@javan.co.id>
2023-05-22 12:48:28 +0700

    Exclude unnecessary files from PHPUnit tests

--- src/App/ConsoleKernel.php
+++ src/App/ConsoleKernel.php
@@ -7,14 +7,6 @@ use Illuminate\Console\Scheduling\Schedule;
 class ConsoleKernel extends \Illuminate\Foundation\Console\Kernel
 {
     /**
-     * Define the application's command schedule.
-     */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-    }
-
-    /**
      * Register the commands for the application.
      */
     protected function commands(): void
