80a6d8a92cf1f4ecdeddc2734b4602d5c0534913
rama <rama@javan.co.id>
2023-05-25 19:39:08 +0700

    Add spatie/laravel-enum, timacdonald/json-api, BlogPost model and BlogPostStatus enum

---
+++ src/Domain/Content/Enums/BlogPostStatus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Domain\Content\Enums;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self DRAFT()
+ * @method static self PUBLISHED()
+ */
+class BlogPostStatus extends Enum
+{
+    public function label(): string
+    {
+        return match($this->value) {
+            self::DRAFT()->value => 'draft',
+            self::PUBLISHED()->value => 'published',
+        };
+    }
+
+    public function color(): string
+    {
+        return match($this->value) {
+            self::DRAFT()->value => 'bg-red-500',
+            self::PUBLISHED()->value => 'bg-green-500',
+        };
+    }
+}
